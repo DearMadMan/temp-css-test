428f2cde39469ca9d32d8e3de0d525dbbdd844e1
wang <i@dearmadman.com>
2015-09-03 17:47:47 +0800

    #

---
+++ scripts/main.js
@@ -0,0 +1,11 @@
+ $(document).ready(function() {
+     //initialize swiper when document ready  
+     var mySwiper = new Swiper('.nav-warp', {
+         // Optional parameters
+         direction: 'vertical',
+     });
+     var dishes = new Swiper('.foods', {
+         // Optional parameters
+         direction: 'vertical',
+     })
+ });
